ee1f9825e204d4ca7db3141c43d2d34b2fb0f3f3
monicaatjes <bakkermoniek@gmail.com>
2020-02-07 20:23:00 +0100

    klad

--- kladje.R
+++ kladje.R
@@ -186,9 +186,9 @@ data$labels_quarters <-data %>%
 x <- c(2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013)
 y_television <- c(74, 82, 80, 74, 73, 72, 74, 70, 70, 66, 66, 69)
 y_internet <- c(13, 14, 20, 24, 20, 24, 24, 40, 35, 41, 43, 50)
-data <- data.frame(x, y_television, y_internet)
+dummydata <- data.frame(x, y_television, y_internet)
 
-### TRIAL PLOTTING
+### TRIAL PLOTTING #########################################################################
 
 
 xaxis <- list(title = "",
@@ -204,84 +204,95 @@ xaxis <- list(title = "",
               ticklen = 5,
               tickfont = list(family = "ING me",
                               size = 12,
-                              color = 'rgb(82, 82, 82)'))
+                              color = 'rgb(204, 204, 204)'))
 
 yaxis <- list(title = "",
               showgrid = FALSE,
               zeroline = FALSE,
               showline = FALSE,
-              showticklabels = FALSE)
-
-margin <- list(autoexpand = FALSE,
-               l = 100,
-               r = 100,
-               t = 110)
+              showticklabels = FALSE,
+              range =c(0,10))
+margin <- list(autoexpand = F)
 
 # Build the annotations
 
-television_1 <- list(
-  xref = 'paper',
-  yref = 'y',
+love_ING_clients <- list(
+  xref = '',
+  yref = '',
   x = 0.05,
-  y = y_television[1],
+  y = love$love_mean_ING_client[1],
   xanchor = 'right',
   yanchor = 'middle',
-  text = ~paste('Television ', y_television[1], '%'),
-  font = list(family = 'Arial',
+  text = ~paste('love ING clients ', love_mean_ING_client[1], '%'),
+  font = list(family = 'ING me',
               size = 16,
               color = 'rgba(67,67,67,1)'),
   showarrow = FALSE)
 
-internet_1 <- list(
-  xref = 'paper',
-  yref = 'y',
+love_mean_ING <- list(
+  xref = '',
+  yref = '',
   x = 0.05,
-  y = y_internet[1],
+  y = love$love_mean_ING[1],
   xanchor = 'right',
   yanchor = 'middle',
-  text = ~paste('Internet ', y_internet[1], '%'),
-  font = list(family = 'Arial',
+  text = ~paste('love ING general pop', love_mean_ING[1], '%'),
+  font = list(family = 'ING me',
               size = 16,
               color = 'rgba(49,130,189, 1)'),
   showarrow = FALSE)
 
-television_2 <- list(
+love_mean_Google <- list(
   xref = 'paper',
   x = 0.95,
-  y = y_television[12],
+  y = love$love_mean_Google[3],
   xanchor = 'left',
   yanchor = 'middle',
-  text = paste('Television ', y_television[12], '%'),
-  font = list(family = 'Arial',
+  text = paste('love Google', love$love_mean_Google[3], '%'),
+  font = list(family = 'ING me',
               size = 16,
               color = 'rgba(67,67,67,1)'),
   showarrow = FALSE)
 
-internet_2 <- list(
-  xref = 'paper',
-  x = 0.95,
-  y = y_internet[12],
-  xanchor = 'left',
-  yanchor = 'middle',
-  text = paste('Internet ', y_internet[12], '%'),
-  font = list(family = 'Arial',
-              size = 16,
-              color = 'rgba(67,67,67,1)'),
-  showarrow = FALSE)
+l<-love %>%
+  plot_ly(x = ~x) %>%
+  add_lines(y = ~love_mean_ING_client, type='scatter', name='love ING clients', 
+            line = list(color = 'rgb(255,098,000)', type='scatter', width = 4, mode = 'markers')) %>%
+  add_lines(y =~love_mean_Google, type='scatter', name='love Google',
+            line = list(color = 'rgb(255,098,000)', width = 4, mode = 'markers')) %>%
+  add_lines(y =~love_mean_ING, type='scatter', name='love ING') %>%
+  layout(title = "love",
+         xaxis = xaxis,
+         yaxis = yaxis)
 
-p <- plot_ly(data, x = ~x) %>%
-  add_trace(y = ~y_television, type = 'scatter', mode = 'lines', line = list(color = 'rgba(67,67,67,1)', width = 2))  %>%
-  add_trace(y = ~y_internet, type = 'scatter', mode = 'lines', line = list(color = 'rgba(49,130,189, 1)', width = 4)) %>%
-  add_trace(x = ~c(x[1], x[12]), y = ~c(y_television[1], y_television[12]), type = 'scatter', mode = 'markers', marker = list(color = 'rgba(67,67,67,1)', size = 8)) %>%
-  add_trace(x = ~c(x[1], x[12]), y = ~c(y_internet[1], y_internet[12]), type = 'scatter', mode = 'markers', marker = list(color = 'rgba(49,130,189, 1)', size = 12)) %>%
-  layout(title = "Main Source for News", xaxis = xaxis, yaxis = yaxis, margin = margin,
-         autosize = FALSE,
-         showlegend = FALSE,
-         annotations = television_1) %>%
-  layout(annotations = internet_1) %>%
-  layout(annotations = television_2) %>%
-  layout(annotations = internet_2)
-p
 
+### NPS ###
+NPS_trial <-result %>%
+  dplyr::filter(!is.na(nps_value)) %>%
+  dplyr::group_by(country, quarter_measurement, b_value) %>%
+  dplyr::mutate(
+    nps_clients = case_when(
+      client_value == 1 ~ nps_value * weight_nps,
+      TRUE ~ NA_real_)
+  ) %>%
+  dplyr::mutate(nps_cat = case_when(
+    nps_clients >=9.0 ~ "promotors",
+    nps_clients <= 9.0 & nps_clients >= 7.0 ~ "neutrals",
+    nps_clients  <= 6.0 ~ "detractors",
+    TRUE ~ "NA_real_")) %>%
+  dplyr::group_by(nps_cat, country, b_value, quarter_measurement) %>%
+  dplyr::tally() %>%
+  dplyr::mutate(
+    percentage = n /sum(n)
+  ) %>% 
+  dplyr::ungroup()
+
+NPS_result <- NPS_trial %>%
+  dplyr::filter(nps_cat %in% c("promotors", "detractors", "neutrals")) %>%
+  dplyr::group_by(country, quarter_measurement, b_value) %>%
+  dplyr::mutate(
+    nps = percentage[nps_cat=="promotors"] - percentage[nps_cat=="detractors"]
+  ) %>%
+  dplyr::ungroup()
 
   
