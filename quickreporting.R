8e51122e4f634c5f1c07538008128fb93b0ffc2e
monicaatjes <bakkermoniek@gmail.com>
2020-06-02 13:11:50 +0200

    useful

--- quickreporting.R
+++ quickreporting.R
@@ -120,7 +120,7 @@ Ready_to_Interpret <- test %>%
   dplyr::select(labels_countries, labels_quarters, quarter_measurement, label, toma, unaided, aided, fami, consideration,
                 preference, desirability, easy_mean, meets_needs_mean, smart_mean, trust4, client, main_bank, pulse,
                 love_mean_ING, love_mean_ING_client, love_mean_Google, price_mean, promotors, detractors, neutrals, 
-                NPS_non_rolling) %>%
+                NPS_non_rolling, rep_trak1, rep_trak2, rep_trak3, rep_trak4) %>%
   dplyr::filter(quarter_measurement >18) %>%
   dplyr::mutate(
     top_of_mind = round(toma *100, digits=2),
@@ -151,7 +151,7 @@ BAM_main_metrics <- Ready_to_Interpret %>%
   dplyr::select(labels_countries, labels_quarters, label, top_of_mind, unaided, aided, familiarity, consideration, 
                 preference, desirability, easy, meets_needs, smart, trust, client, main_bank, pulse_BAM, 
                 love_mean_ING, love_mean_ING_client, love_mean_Google, price_perception, promotors, neutrals, 
-                detractors, NPS_non_rolling)
+                detractors, NPS_non_rolling, rep_trak1, rep_trak2, rep_trak3, rep_trak4)
   
 BAM_main_metrics <-write.csv(BAM_main_metrics, "BAM_main_metrics.csv")
 
@@ -330,5 +330,278 @@ tempREP211 <- result %>%
 
 data1 <- full_join(data1, tempREP211, by=c("b_value", "country", "quarter_measurement"))
 rm(tempREP11)
+
+
+## split for app users
+App_usage <- result %>%
+  dplyr::select(weight, country, quarter_measurement, b_value, nps_value, client_value, app_usage_value, main_bank_value) %>%
+  #dplyr::filter(country ==9) %>%
+  dplyr::filter(!is.na(nps_value) & client_value==1 & !is.na(app_usage_value)) %>%
+  dplyr::mutate(app_usage = case_when(
+    app_usage_value==1 ~ "Yes, at least five times",
+    app_usage_value==2 ~ "Yes, but not very often, fewer than five times",
+    app_usage_value==3 ~ "No, but I do have the app on my smartphone",
+    app_usage_value==4 ~ "No, and I don’t have the app on my smartphone either",
+    app_usage_value==5 ~ "No, I don’t have a smartphone",
+    TRUE ~ "NA_real_"
+  )) %>%
+  dplyr::group_by(country, quarter_measurement, b_value, app_usage) %>%
+  dplyr::tally(wt=weight) %>%
+  dplyr::mutate(
+    percentage = n /sum(n) *100
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(country, quarter_measurement, b_value, app_usage, percentage) %>%
+  dplyr::distinct() %>%
+  #tibble::rowid_to_column() %>%
+  tidyr::spread(app_usage, percentage) 
+
+# to add Q figures to data set, split for clients
+data1 <- full_join(data1, App_usage, by=c("b_value", "country", "quarter_measurement"))
+
+# version to work with
+App_usage <- result %>%
+  dplyr::select(weight, country, quarter_measurement, b_value, nps_value, client_value, app_usage_value, main_bank_value) %>%
+  #dplyr::filter(country ==9) %>%
+  dplyr::filter(!is.na(nps_value) & client_value==1 & !is.na(app_usage_value)) %>%
+  dplyr::mutate(app_usage = case_when(
+    app_usage_value==1 ~ "Yes, at least five times",
+    app_usage_value==2 ~ "Yes, but not very often, fewer than five times",
+    app_usage_value==3 ~ "No, but I do have the app on my smartphone",
+    app_usage_value==4 ~ "No, and I don’t have the app on my smartphone either",
+    app_usage_value==5 ~ "No, I don’t have a smartphone",
+    TRUE ~ "NA_real_"
+  )) %>%
+  dplyr::group_by(country, quarter_measurement, b_value, app_usage, main_bank_value) %>%
+  dplyr::tally(wt=weight) %>%
+  dplyr::mutate(
+    percentage = n /sum(n) *100
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(country, quarter_measurement, b_value, app_usage, percentage, main_bank_value) %>%
+  dplyr::distinct() %>%
+  #tibble::rowid_to_column() %>%
+  tidyr::spread(app_usage, percentage) 
+
+## NL
+overview_app_usage <- data1 %>%
+  dplyr::select("Yes, but not very often, fewer than five times", "Yes, at least five times", "No, I don’t have a smartphone",
+                "No, but I do have the app on my smartphone", "No, and I don’t have the app on my smartphone either",
+                labels_countries, labels_quarters, label, quarter_measurement, b_value, country, NPS_non_rolling,
+                main_bank) %>%
+  #dplyr::filter(country==9) %>%
+  dplyr::filter(b_value <5)
+
+## overview plaatje
+overview_app_usage <-overview_app_usage %>%
+  dplyr::filter(country==9)
+
+p1_NL <- plot_ly(overview_app_usage, x = ~label, y = ~overview_app_usage$`Yes, at least five times`, type = 'bar', name = 'Yes, at least five times', 
+              marker = list(color = 'rgb(255,098,000)')) %>%
+  add_trace(y = ~overview_app_usage$`Yes, but not very often, fewer than five times`, name = "Yes, but not very often, fewer than five times", 
+            marker = list(color = 'rgb(52,150,81)')) %>%
+  add_trace(y = ~overview_app_usage$`No, but I do have the app on my smartphone`, name = "No, but I do have the app on my smartphone", 
+            marker = list(color = 'rgb(96,166,218)')) %>%
+  add_trace(y = ~overview_app_usage$`No, and I don’t have the app on my smartphone either`, name = "No, and I don’t have the app on my smartphone either", 
+            marker = list(color = 'rgb(82,81,153)')) %>%
+  add_trace(y = ~overview_app_usage$`No, I don’t have a smartphone`, name = "No, I don’t have a smartphone", 
+            marker = list(color = "rgb 168, 168, 168")) %>%
+  layout(xaxis = xaxis,
+         yaxis = yaxis,
+         #margin = list(b = 100),
+         barmode = 'stack', title= "",
+         legend =list(font = list(family = "ING me", size = 12, color = 'rgb(105, 105, 105)')))
+p1_NL
+
+## BE
+overview_app_usage <- data1 %>%
+  dplyr::select("Yes, but not very often, fewer than five times", "Yes, at least five times", "No, I don’t have a smartphone",
+                "No, but I do have the app on my smartphone", "No, and I don’t have the app on my smartphone either",
+                labels_countries, labels_quarters, label, quarter_measurement, b_value, country, NPS_non_rolling,
+                main_bank) %>%
+  #dplyr::filter(country==9) %>%
+  dplyr::filter(b_value <5)
+
+
+overview_app_usage <-overview_app_usage %>%
+  dplyr::filter(country==3)
+p1_BE <- plot_ly(overview_app_usage, x = ~label, y = ~overview_app_usage$`Yes, at least five times`, type = 'bar', name = 'Yes, at least five times', 
+              marker = list(color = 'rgb(255,098,000)')) %>%
+  add_trace(y = ~overview_app_usage$`Yes, but not very often, fewer than five times`, name = "Yes, but not very often, fewer than five times", 
+            marker = list(color = 'rgb(52,150,81)')) %>%
+  add_trace(y = ~overview_app_usage$`No, but I do have the app on my smartphone`, name = "No, but I do have the app on my smartphone", 
+            marker = list(color = 'rgb(96,166,218)')) %>%
+  add_trace(y = ~overview_app_usage$`No, and I don’t have the app on my smartphone either`, name = "No, and I don’t have the app on my smartphone either", 
+            marker = list(color = 'rgb(82,81,153)')) %>%
+  add_trace(y = ~overview_app_usage$`No, I don’t have a smartphone`, name = "No, I don’t have a smartphone", 
+            marker = list(color = "rgb 168, 168, 168")) %>%
+  layout(xaxis = xaxis,
+         yaxis = yaxis,
+         #margin = list(b = 100),
+         barmode = 'stack', title= "",
+         legend =list(font = list(family = "ING me", size = 12, color = 'rgb(105, 105, 105)')))
+p1_BE
+
+
+
+### NPS
+NPS_trial_ORI <- result %>%
+  dplyr::select(weight, country, quarter_measurement, b_value, nps_value, client_value, app_usage_value, main_bank_value) %>%
+  dplyr::filter(!is.na(nps_value) & client_value==1) %>% 
+  dplyr::filter(b_value<5) %>%
+  #dplyr::filter(app_usage_value==1|app_usage_value==2) %>%
+  dplyr::filter(country ==9|country==3) %>%
+  dplyr::mutate(nps_cat = case_when(
+    nps_value >= 9 ~ "promotors",
+    nps_value < 9 & nps_value > 6 ~ "neutrals",
+    nps_value  <= 6 ~ "detractors",
+    TRUE ~ "NA_real_")) %>%
+  #dplyr::mutate(app_users = case_when(
+  #  app_usage_value <3 ~ 1,
+  #  TRUE ~ 0
+ # )) %>%
+  dplyr::group_by(country, quarter_measurement, b_value, nps_cat) %>%
+  dplyr::tally(wt=weight) %>%
+  dplyr::mutate(
+    percentage = n /sum(n) *100
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(country, quarter_measurement, b_value, nps_cat, percentage) %>%
+  tidyr::spread(nps_cat, percentage) %>%
+  dplyr::mutate(
+    NPS_non_rolling =round(promotors, digits =2)- round(detractors, digits =2)
+  ) 
+
+NPS_trial_APP <- result %>%
+  dplyr::select(weight, country, quarter_measurement, b_value, nps_value, client_value, app_usage_value, main_bank_value) %>%
+  dplyr::filter(!is.na(nps_value) & client_value==1) %>% 
+  dplyr::filter(b_value<5) %>%
+  dplyr::filter(app_usage_value==1|app_usage_value==2) %>%
+  dplyr::filter(country ==9|country==3) %>%
+  dplyr::mutate(nps_cat = case_when(
+    nps_value >= 9 ~ "promotors_APP",
+    nps_value < 9 & nps_value > 6 ~ "neutrals_APP",
+    nps_value  <= 6 ~ "detractors_APP",
+    TRUE ~ "NA_real_")) %>%
+  #dplyr::mutate(app_users = case_when(
+  #  app_usage_value <3 ~ 1,
+  #  TRUE ~ 0
+  # )) %>%
+  dplyr::group_by(country, quarter_measurement, b_value, nps_cat) %>%
+  dplyr::tally(wt=weight) %>%
+  dplyr::mutate(
+    percentage = n /sum(n) *100
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(country, quarter_measurement, b_value, nps_cat, percentage) %>%
+  tidyr::spread(nps_cat, percentage) %>%
+  dplyr::mutate(
+    NPS_non_rolling_APP =round(promotors_APP, digits =2)- round(detractors_APP, digits =2)
+  ) 
+
+
+NPS_trial_MAPP <- result %>%
+  dplyr::select(weight, country, quarter_measurement, b_value, nps_value, client_value, app_usage_value, main_bank_value) %>%
+  dplyr::filter(!is.na(nps_value) & main_bank_value==1) %>% 
+  dplyr::filter(b_value<5) %>%
+  dplyr::filter(app_usage_value==1|app_usage_value==2) %>%
+  dplyr::filter(country ==9|country==3) %>%
+  dplyr::mutate(nps_cat = case_when(
+    nps_value >= 9 ~ "promotors_MAPP",
+    nps_value < 9 & nps_value > 6 ~ "neutrals_MAPP",
+    nps_value  <= 6 ~ "detractors_MAPP",
+    TRUE ~ "NA_real_")) %>%
+  #dplyr::mutate(app_users = case_when(
+  #  app_usage_value <3 ~ 1,
+  #  TRUE ~ 0
+  # )) %>%
+  dplyr::group_by(country, quarter_measurement, b_value, nps_cat) %>%
+  dplyr::tally(wt=weight) %>%
+  dplyr::mutate(
+    percentage = n /sum(n) *100
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(country, quarter_measurement, b_value, nps_cat, percentage) %>%
+  tidyr::spread(nps_cat, percentage) %>%
+  dplyr::mutate(
+    NPS_non_rolling_MAPP =round(promotors_MAPP, digits =2)- round(detractors_MAPP, digits =2)
+  ) 
+
+NPS_NL_BE <- dplyr::left_join(NPS_trial_ORI, NPS_trial_APP, b=c("country", "quarter_measurement", "b_value"))
+
+NPS_NL_BE <- dplyr::left_join(NPS_NL_BE, NPS_trial_MAPP, b=c("country", "quarter_measurement", "b_value"))
+
+## plot bandwith pulse
+
+rep <- test %>%
+  dplyr::select(quarter_measurement, country, b_value, pulse, label, labels_countries) %>%
+  dplyr::filter(quarter_measurement== max(quarter_measurement)) %>%
+  dplyr::filter(!is.na(pulse)) %>%
+  dplyr::filter(country!=3) %>%
+  dplyr::filter(country!=6) %>%
+  dplyr::filter(country!=9) %>%
+  dplyr::mutate(pulse = case_when(
+    country ==14 & quarter_measurement==19 ~ pulse /1.603,
+    TRUE ~ pulse
+  )) %>%
+  dplyr::group_by(labels_countries) %>%
+  dplyr::mutate(
+    min_pulse = min(pulse),
+    max_pulse = max(pulse),
+    pulse_ING = case_when(
+      b_value ==1 ~ pulse,
+      TRUE ~ NA_real_
+    )
+  )
+
+xaxis <- list(title = "countries", 
+              titlefont = list(color = 'rgb(105, 105, 105)', size = 12, family = "ING me"),
+              #range =c(20,80),
+              #dtick = 10,
+              showline = TRUE,
+              showgrid = TRUE,
+              linecolor = 'rgb(204, 204, 204)',
+              linewidth = 2,
+              tickfont = list(family = "ING me",
+                              size = 12,
+                              color = 'rgb(105, 105, 105)'))
+yaxis <- list(title = "pulse",
+              titlefont = list(color = 'rgb(105, 105, 105)', size = 12, family = "ING me"),
+              range =c(30,100),
+              showticklabels = TRUE,
+              linecolor = 'rgb(204, 204, 204)',
+              linewidth = 2,
+              autotick = TRUE,
+              #dtick = 5,
+              ticks = 'inside',
+              tickcolor = 'rgb(204, 204, 204)',
+              tickwidth = 1,
+              #ticklen = 1,
+              tickfont = list(family = "ING me",
+                              size = 12,
+                              color = 'rgb(105, 105, 105)'))
+
+p <- rep %>%
+  plot_ly(x = ~labels_countries, y= rep$pulse, type = 'scatter', color ='rgb(255,098,000)',
+          mode = 'text+markers', text = ~label, textposition = 'middle right',
+          textfont = list(family = "ING me", color = 'rgb(105, 105, 105)', size = 11)) %>%
+  layout(title = '',
+         xaxis = xaxis,
+         yaxis = yaxis)
+
+p
+
+p <- rep %>%
+  plot_ly(x = ~labels_countries, y= rep$min_pulse, type = 'scatter', mode= 'markers',
+          name = 'min', marker = list(color = 'rgb(52,150,81)', type='scatter', width = 4, mode = 'markers')) %>%
+  add_trace(y = ~rep$max_pulse, type = 'scatter', mode= 'markers',
+            name = 'max', marker = list(color = 'rgb(96,166,218)', type='scatter', width = 4, mode = 'markers')) %>%
+  add_trace(y= ~rep$pulse_ING, type = 'scatter', mode= 'markers',
+            name = 'ING', marker = list(color = 'rgb(255,098,000)', type='scatter', width = 4, mode = 'markers')) %>%
+  layout(title = '',
+         xaxis = xaxis,
+         yaxis = yaxis)
+
+p
 
 
